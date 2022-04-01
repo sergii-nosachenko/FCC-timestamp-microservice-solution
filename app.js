904631b9dd8ace91fd49ea3c3ff0f0627b13a708
sergii-nosachenko <s.nosachenko@gmail.com>
2022-04-01 15:01:30 +0300

    Bug fixes

--- app.js
+++ app.js
@@ -1,40 +1,23 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
+const parser = require('./parseDate');
 
 const app = express();
 
+const port = process.env.PORT || 5000;
+
 // API MIDDLEWARE
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC
+
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 app.use('/public', express.static('public'));
 
-app.use('/api/:date', (req, res, next) => {
-  // Check the date param string
-
-  /* If it's a UNIX timestamp - convert it to an integer:
-      new Date() constructor accepts Number and String values
-      in different ways.
-      As request parameters are strings, we need to convert it
-      to Number before using as argument.
-      In other case we'll get an Invalid Date object!
-  */
-  const date = req.params.date
-    ? req.params.date == +req.params.date
-      ? new Date(+req.params.date)
-      : new Date(req.params.date)
-    : new Date(date);
-
-  if (date.toString() == 'Invalid Date') {
-    req.json = {
-      err: date.toString()
-    };
-  } else {
-    req.json = {
-      unix: date.valueOf(),
-      utc: date.toUTCString()
-    };
-  }
-  next();
-});
+app.use('/api', parser);
+app.use('/api/:date', parser);
 
 // API ROUTES
 
@@ -44,11 +27,14 @@ app.get('/', (req, res) => {
 });
 
 // API GET route
+app.get('/api', (req, res) => {
+  res.json(req.json);
+});
 app.get('/api/:date', (req, res) => {
   res.json(req.json);
 });
 
 // SERVER RUN
-app.listen(process.env.PORT, () => {
-  console.log('Server running on port: ', process.env.PORT);
+app.listen(port, () => {
+  console.log('Server running on port: ', port);
 });
---
+++ parseDate.js
@@ -0,0 +1,30 @@
+// Check the date param string
+const parseDate = (req, res, next) => {
+  /*  If it's a UNIX timestamp - convert it to an integer:
+      new Date() constructor accepts Number and String values
+      in different ways.
+      As request parameters are strings, we need to convert it
+      to Number before using as argument.
+      In other case we'll get an Invalid Date object!
+  */
+
+  const date = req.params.date
+    ? req.params.date == +req.params.date
+      ? new Date(+req.params.date)
+      : new Date(req.params.date)
+    : new Date();
+
+  if (date.toString() == 'Invalid Date') {
+    req.json = {
+      err: date.toString()
+    };
+  } else {
+    req.json = {
+      unix: date.valueOf(),
+      utc: date.toUTCString()
+    };
+  }
+  next();
+};
+
+module.exports = parseDate;
